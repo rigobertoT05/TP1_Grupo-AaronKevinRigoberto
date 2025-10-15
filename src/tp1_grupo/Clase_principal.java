184d0e63ae25102a8151f808ef97f91dc46da89e
Kevin Rugama17 <kevinrugama171@gmail.com>
2025-10-15 15:58:27 -0600

    esta es la preueba

---
+++ src/tp1_grupo/Clase_principal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1_grupo;
+
+/**
+ *
+ * @author kevin
+ */
+public class Clase_principal {
+    
+   //esto es una prueba
+  
+    
+}
