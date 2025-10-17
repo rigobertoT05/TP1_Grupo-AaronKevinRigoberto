8dfd05ec7e3d55425926bdd4d0c6c6ae2011707f
AaronR <aaroncastrillo123@gmail.com>
2025-10-16 18:13:12 -0600

    Cambio en calcular tarifa2

---
+++ src/tp1_grupo/Bicicleta.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp1_grupo;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author rigoberto
+ */
+public class Bicicleta extends Vehiculo {
+    private int cambios;
+    private String tipoBicicleta;
+    public Bicicleta(String marca, String placa,String tipo, LocalDateTime horaEntrada, LocalDateTime horaSalida, int cambios, String tipoBicicleta ) {
+        super(marca, placa,"Bicicleta", horaEntrada, horaSalida);
+        this.cambios = cambios;
+        this.tipoBicicleta = tipoBicicleta;
+    }
+
+    @Override
+    public double calcularTarifa() {
+        long horas = calcularHorasParqueo();
+        return horas * 300;
+    }
+     public String toString() {
+    return "Bicicleta - Marca: " + getMarca() +
+           ", Placa: " + getPlaca() + ", Cambios: " + cambios + ", Tipo: " + tipo;}
+
+}
